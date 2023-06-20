f2499ebdecc607172a0b93fb76f5ec0319c33d4f
NiFernandes2103 <nff3@illinois.edu>
2023-06-20 20:15:34 -0300

    First results and fixed fileFunc.cpp segmentation error

--- KT-ISshear/C++/fileFunc.cpp
+++ KT-ISshear/C++/fileFunc.cpp
@@ -14,26 +14,26 @@ void create(state initial,string filename) {
     // opens an existing csv file or creates a new file.
     fout.open(filename, ios::out | ios::app);
 
-    vector<vector<double>> rho  = initial.get(1);        
-    vector<vector<double>> Momx = initial.get(2);
-    vector<vector<double>> Momy = initial.get(3);
-    vector<vector<double>> Pixx = initial.get(4);
-    vector<vector<double>> Pixy = initial.get(5);
-    vector<vector<double>> Piyx = initial.get(6);
-    vector<vector<double>> Piyy = initial.get(7);
+    vector<vector<double>> rhoIC  = initial.get(0);        
+    vector<vector<double>> MomxIC = initial.get(1);
+    vector<vector<double>> MomyIC = initial.get(2);
+    vector<vector<double>> PixxIC = initial.get(3);
+    vector<vector<double>> PixyIC = initial.get(4);
+    vector<vector<double>> PiyxIC = initial.get(5);
+    vector<vector<double>> PiyyIC = initial.get(6);
 
-    int N = rho.size();
+    int A = rhoIC.size();
 
     // Read the input
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            fout << rho[i][j]  <<',';
-            fout << Momx[i][j] <<',';
-            fout << Momy[i][j] <<',';
-            fout << Pixx[i][j] <<',';
-            fout << Pixy[i][j] <<',';
-            fout << Piyx[i][j] <<',';
-            fout << Piyy[i][j] <<',';
+    for (int i = 0; i < A; i++) {
+        for (int j = 0; j < A; j++) {
+            fout << rhoIC[i][j]  <<',';
+            fout << MomxIC[i][j] <<',';
+            fout << MomyIC[i][j] <<',';
+            fout << PixxIC[i][j] <<',';
+            fout << PixyIC[i][j] <<',';
+            fout << PiyxIC[i][j] <<',';
+            fout << PiyyIC[i][j] <<',';
         }
     }
     
@@ -50,13 +50,13 @@ void write(map<double, state> solution, string filename)
 
     // Iterate over the list using the iterator
     for (map<double,state>::iterator it = solution.begin(); it != solution.end(); ++it) {
-        vector<vector<double>> rho  = (*it).second.get(1);        
-        vector<vector<double>> Momx = (*it).second.get(2);
-        vector<vector<double>> Momy = (*it).second.get(3);
-        vector<vector<double>> Pixx = (*it).second.get(4);
-        vector<vector<double>> Pixy = (*it).second.get(5);
-        vector<vector<double>> Piyx = (*it).second.get(6);
-        vector<vector<double>> Piyy = (*it).second.get(7);
+        vector<vector<double>> rho  = (*it).second.get(0);        
+        vector<vector<double>> Momx = (*it).second.get(1);
+        vector<vector<double>> Momy = (*it).second.get(2);
+        vector<vector<double>> Pixx = (*it).second.get(3);
+        vector<vector<double>> Pixy = (*it).second.get(4);
+        vector<vector<double>> Piyx = (*it).second.get(5);
+        vector<vector<double>> Piyy = (*it).second.get(6);
 
         int N = rho.size();
 
